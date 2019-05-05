5a1bed4a6bf0b9bdf02908a71e5e765745fb80ff
Chase <ccviol01@gmail.com>
2019-05-04 22:38:08 -0700

    3 light pattern

---
+++ 3_lights_pattern.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+for x in range(0, 100):
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(18,GPIO.LOW)
+    GPIO.output(23,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(23,GPIO.LOW)
+    GPIO.output(25,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(25,GPIO.LOW)
+    time.sleep(1)
+
+    # all on
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(23,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(25,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(18,GPIO.LOW)
+    GPIO.output(23,GPIO.LOW)
+    GPIO.output(25,GPIO.LOW)
+    time.sleep(1)
