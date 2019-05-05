784ba04e1ef585de9fd9d59cadeb90ac72e21bce
Chase <ccviol01@gmail.com>
2019-05-04 18:26:23 -0700

    more lights

--- light_on.py
+++ light_on.py
@@ -2,11 +2,16 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-for x in range(0, 100) :
-    GPIO.setup(18,GPIO.OUT)
-    print("LED on")
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+for x in range(0, 100):
+    print("LED 18 on")
     GPIO.output(18,GPIO.HIGH)
+    time.sleep(1)
+    print("LED 23 on")
+    GPIO.output(23,GPIO.HIGH)
     time.sleep(1)
     print("LED off")
     GPIO.output(18,GPIO.LOW)
+    GPIO.output(23,GPIO.LOW)
     time.sleep(1)
